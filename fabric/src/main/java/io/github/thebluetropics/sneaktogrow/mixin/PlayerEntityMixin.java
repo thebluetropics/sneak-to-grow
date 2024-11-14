eca8c36af6421977510ca64a12baee6d7cef7955
thebluetropics <thebluetropics@gmail.com>
2024-11-14 16:45:42 +0700

    refactor `PlayerEntityMixin` class
    make it more readable and expand the growable area to 3x3x3

--- fabric/src/main/java/io/github/thebluetropics/sneaktogrow/mixin/PlayerEntityMixin.java
+++ fabric/src/main/java/io/github/thebluetropics/sneaktogrow/mixin/PlayerEntityMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.HashSet;
 import java.util.Objects;
 
 @Mixin(PlayerEntity.class)
@@ -25,37 +27,58 @@ public class PlayerEntityMixin {
     var world = player.getWorld();
 
     if (!world.isClient && player.isSneaking() && !isTickProcessed) {
-      BlockPos origin = player.getBlockPos().subtract(new Vec3i(1, 0, 1));
+      BlockPos origin = player.getBlockPos().subtract(new Vec3i(1, 1, 1));
+
+      HashSet<BlockPos> growableBlockPosSet = new HashSet<>();
 
       for (int x = 0; x < 3; x++) {
-        for (int z = 0; z < 3; z++) {
-          BlockPos blockPos = origin.add(x, 0, z);
-          BlockState blockState = world.getBlockState(blockPos);
+        for (int y = 0; y < 3; y++) {
+          for (int z = 0; z < 3; z++) {
+            BlockPos blockPos = origin.add(x, y, z);
+            BlockState blockState = world.getBlockState(blockPos);
 
-          if (blockState.getBlock() instanceof SaplingBlock) {
-            if (Objects.equals(world.random.nextInt(16), 0)) {
-              BoneMealItem.useOnFertilizable(new ItemStack(Items.BONE_MEAL), world, blockPos);
-            }
-          }
-
-          if (blockState.getBlock() instanceof CropBlock) {
-            if (Objects.equals(world.random.nextInt(16), 0)) {
+            if (blockState.getBlock() instanceof CropBlock) {
               if (blockState.get(CropBlock.AGE) < CropBlock.MAX_AGE) {
-                world.setBlockState(blockPos, blockState.with(CropBlock.AGE, blockState.get(CropBlock.AGE) + 1), Block.NOTIFY_LISTENERS);
+                growableBlockPosSet.add(blockPos);
+              }
+            }
+
+            if (blockState.getBlock() instanceof SaplingBlock) {
+              growableBlockPosSet.add(blockPos);
+            }
+
+            if (blockState.getBlock() instanceof SweetBerryBushBlock) {
+              if (blockState.get(SweetBerryBushBlock.AGE) < SweetBerryBushBlock.MAX_AGE) {
+                growableBlockPosSet.add(blockPos);
               }
             }
           }
+        }
+      }
 
-          if (blockState.getBlock() instanceof SweetBerryBushBlock) {
-            if (blockState.get(SweetBerryBushBlock.AGE) < 3) {
-              if (Objects.equals(world.random.nextInt(16), 0)) {
-                world.setBlockState(
-                  blockPos,
-                  blockState.with(SweetBerryBushBlock.AGE, blockState.get(SweetBerryBushBlock.AGE) + 1),
-                  Block.NOTIFY_LISTENERS
-                );
-              }
-            }
+      if (!growableBlockPosSet.isEmpty()) {
+        BlockPos selectedBlockPos = growableBlockPosSet.toArray(new BlockPos[]{})[world.getRandom().nextInt(growableBlockPosSet.size())];
+        BlockState selectedBlockState = world.getBlockState(selectedBlockPos);
+
+        if (selectedBlockState.getBlock() instanceof CropBlock) {
+          if (Objects.equals(world.random.nextInt(16), 0)) {
+            world.setBlockState(selectedBlockPos, selectedBlockState.with(CropBlock.AGE, selectedBlockState.get(CropBlock.AGE) + 1), Block.NOTIFY_LISTENERS);
+          }
+        }
+
+        if (selectedBlockState.getBlock() instanceof SaplingBlock) {
+          if (Objects.equals(world.random.nextInt(24), 0)) {
+            BoneMealItem.useOnFertilizable(new ItemStack(Items.BONE_MEAL), world, selectedBlockPos);
+          }
+        }
+
+        if (selectedBlockState.getBlock() instanceof SweetBerryBushBlock) {
+          if (Objects.equals(world.random.nextInt(24), 0)) {
+            world.setBlockState(
+              selectedBlockPos,
+              selectedBlockState.with(SweetBerryBushBlock.AGE, selectedBlockState.get(SweetBerryBushBlock.AGE) + 1),
+              Block.NOTIFY_LISTENERS
+            );
           }
         }
       }
